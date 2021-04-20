dae1e009cd716f7931a6e9f9702ef766a4b3640b
MnoZombie956 <34872596+MnoZombie956@users.noreply.github.com>
2021-04-19 22:49:07 -0300

    Update dijkstra_GraphSolver.c

--- dijkstra_GraphSolver.c
+++ dijkstra_GraphSolver.c
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
- * Author: ofzbo
+ * Author: marco956
  *
  * Created on 29 de fevereiro de 2020, 12:17
  */
